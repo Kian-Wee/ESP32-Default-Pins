f838e9e9f9bd5fa16ed79a1cea82c8403d6d3701
KW <45945080+Kian-Wee@users.noreply.github.com>
2022-11-29 14:51:41 +0800

    hi james

---
+++ ESP32_Default_Pins/ESP32_Default_Pins.ino
@@ -0,0 +1,21 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.print("MOSI: ");
+  Serial.println(MOSI);
+  Serial.print("MISO: ");
+  Serial.println(MISO);
+  Serial.print("SCK: ");
+  Serial.println(SCK);
+  Serial.print("SS: ");
+  Serial.println(SS); 
+  Serial.print("SDA: ");
+  Serial.println(SDA);
+  Serial.print("SCL: ");
+  Serial.println(SCL);
+  delay(2000);
+}
